8bb82226f71bf0d5ef9059e37056a8fbcefe19a7
Calvin Rai <raicalvin@gmail.com>
2019-01-02 20:33:35 -0800

    Created initial action constants

---
+++ actions/index.js
@@ -0,0 +1,13 @@
+/* 
+    1. Getting all the decks from the store/storage
+    2. Creatnig a new deck
+    3. Creating a new card
+    4. Deleting a card
+    5. Deleting a deck
+*/
+
+export const CREATE_DECK = "CREATE_DECK"; // Create a new deck
+export const ADD_CARD = "ADD_CARD"; // Add a new question card
+export const GET_DECKS = "GET_DECKS"; // Get all the decks to view on screen
+export const DELETE_CARD = "DELETE_CARD"; // Delete a question card
+export const DELETE_DECK = "DELETE_DECK"; // Delete a specific deck
