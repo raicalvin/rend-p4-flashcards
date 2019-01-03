495f73088d37da0152f199f4df47fec10c33b54b
Calvin Rai <raicalvin@gmail.com>
2019-01-02 20:23:34 -0800

    Created initial DeckView component

---
+++ components/DeckView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class DeckView extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Deck View Main Screen!</Text>
+      </View>
+    );
+  }
+}
+
+export default DeckView;
