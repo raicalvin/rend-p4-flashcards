2fcad2732495d2e24fcffad19ef2cdb433784b23
Calvin Rai <raicalvin@gmail.com>
2019-01-02 20:52:10 -0800

    Created initial action creators

--- actions/index.js
+++ actions/index.js
@@ -6,8 +6,47 @@
     5. Deleting a deck
 */
 
+// ACTIONS
+
 export const CREATE_DECK = "CREATE_DECK"; // Create a new deck
 export const ADD_CARD = "ADD_CARD"; // Add a new question card
 export const GET_DECKS = "GET_DECKS"; // Get all the decks to view on screen
 export const DELETE_CARD = "DELETE_CARD"; // Delete a question card
 export const DELETE_DECK = "DELETE_DECK"; // Delete a specific deck
+
+// ACTION CREATORS
+
+export function getDecks(decks) {
+  return {
+    type: GET_DECKS,
+    decks
+  };
+}
+
+export function addDeck(deck) {
+  return {
+    type: CREATE_DECK,
+    deck
+  };
+}
+
+export function deleteDeck(deck) {
+  return {
+    type: DELETE_DECK,
+    deck
+  };
+}
+
+export function addCard(card) {
+  return {
+    type: ADD_CARD,
+    card
+  };
+}
+
+export function deleteCard(card) {
+  return {
+    type: DELETE_DECK,
+    card
+  };
+}
