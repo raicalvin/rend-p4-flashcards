cd6784b2fab5af931d7fe154acdd855604aca532
Calvin Rai <raicalvin@gmail.com>
2019-01-03 16:32:01 -0800

    Added name and question length to Deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { getInitialDecks } from "../utils/api";
 
 class Deck extends Component {
   render() {
+    const deck = this.props.navigation.state.params.entryId;
+    const decks = getInitialDecks();
     return (
       <View style={styles.container}>
-        <Text>This is the individual DECK</Text>
+        <Text>{decks[deck].name}</Text>
+        <Text>{decks[deck].questions.length}</Text>
       </View>
     );
   }
