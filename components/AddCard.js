7bddb86fed3d6f3597c39e0e1e167c929ab4aecb
Calvin Rai <raicalvin@gmail.com>
2019-01-04 15:42:09 -0800

    Updated AddCard component with Text Input and buttons

--- components/AddCard.js
+++ components/AddCard.js
@@ -13,8 +13,46 @@ import {
 } from "react-native";
 
 class AddCard extends Componet {
+  state = {
+    questionText: "",
+    answerText: "",
+    correctAnswerText: ""
+  };
+
+  submitCard = () => {};
+
   render() {
-    return <View />;
+    const deckName = this.props.navigation.state.params.entryId;
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.title} />
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.setState({ questionText: text })}
+            value={this.state.questionText}
+          />
+          <Text />
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.setState({ answerText: text })}
+            value={this.state.answerText}
+          />
+          <Text />
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.setState({ correctAnswerText: text })}
+            value={this.state.correctAnswerText}
+          />
+          <TouchableOpacity
+            style={styles.submitBtn}
+            onPress={() => this.submitCard(deckName)}
+          >
+            <Text style={styles.submitBtnText}>Submit!</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
   }
 }
 
@@ -23,5 +61,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    testAlign: "center"
+  },
+  title: {
+    fontSize: 30,
+    color: "#333"
+  },
+  submitBtn: {
+    borderWidth: 0.5,
+    borderColor: "#6d67da",
+    padding: 10,
+    backgroundColor: orange,
+    borderRadius: 7,
+    overflow: "hidden"
+  },
+  input: {
+    width: 250,
+    height: 40,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#757575",
+    margin: 20,
+    borderRadius: 7
   }
 });
