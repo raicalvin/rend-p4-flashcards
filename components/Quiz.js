32ad92ce61c556d976d4cde12b63b0967a9ced18
Calvin Rai <raicalvin@gmail.com>
2019-01-04 20:10:34 -0800

    Added styles for Quiz section of the app

--- components/Quiz.js
+++ components/Quiz.js
@@ -25,14 +25,17 @@ class Quiz extends Component {
     const number = this.state.questionNumber + 1;
     return (
       <View style={styles.container}>
-        <Text>
-          {number} / {decks[currentDeck].questions.length}
-        </Text>
-
-        <Text>{decks[currentDeck].questions[questionNumber].question}</Text>
-        <QuizInfo text="Show Answer" />
-        <MainButton styles={styles} text="Correct" />
-        <MainButton styles={styles} text="Incorrect" />
+        <View style={styles.card}>
+          <Text style={styles.questions}>
+            {number} / {decks[currentDeck].questions.length}
+          </Text>
+          <Text style={styles.mainText}>
+            {decks[currentDeck].questions[questionNumber].question}
+          </Text>
+          <QuizInfo style={styles.answer} text="Show Answer" />
+          <MainButton color={green} styles={styles} text="Correct" />
+          <MainButton color={red} styles={styles} text="Incorrect" />
+        </View>
       </View>
     );
   }
@@ -50,6 +53,47 @@ const styles = StyleSheet.create({
     height: 45,
     margin: 5,
     width: 160
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 26,
+    textAlign: "center"
+  },
+  questions: {
+    top: 0,
+    alignSelf: "flex-start",
+    left: 0,
+    color: white,
+    fontSize: 20,
+    margin: 5,
+    position: "absolute"
+  },
+  answer: {
+    color: white,
+    fontSize: 20,
+    margin: 20
+  },
+  card: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "orange",
+    alignSelf: "stretch",
+    borderRadius: 10,
+    shadowColor: "rgba(0, 0, 0, 0.34)",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 4,
+    shadowOpacity: 1
+  },
+  mainText: {
+    fontSize: 40,
+    color: white,
+    marginTop: 40,
+    textAlign: "center"
   }
 });
 
