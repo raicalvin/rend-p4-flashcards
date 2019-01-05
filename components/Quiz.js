969c7cb49e774d3b11133ddd2840d3a1ab2f50dd
Calvin Rai <raicalvin@gmail.com>
2019-01-04 20:00:56 -0800

    Added text information for QuizInfo

--- components/Quiz.js
+++ components/Quiz.js
@@ -11,12 +11,28 @@ import { orange, white, purple, red, green } from "../utils/colors";
 import SubmitButton from "./SubmitButton";
 import { connect } from "react-redux";
 import MainButton from "./MainButton";
+import QuizInfo from "./QuizInfo";
 
 class Quiz extends Component {
+  state = {
+    questionNumber: 0
+  };
+
   render() {
+    const decks = this.props.decks;
+    const currentDeck = this.props.navigation.state.params.entryId;
+    const questionNumber = this.state.questionNumber;
+    const number = this.state.questionNumber + 1;
     return (
       <View style={styles.container}>
-        <Text>Hello from Quiz Component!</Text>
+        <Text>
+          {number} / {decks[currentDeck].questions.length}
+        </Text>
+
+        <Text>{decks[currentDeck].questions[questionNumber].question}</Text>
+        <QuizInfo text="Show Answer" />
+        <MainButton styles={styles} text="Correct" />
+        <MainButton styles={styles} text="Incorrect" />
       </View>
     );
   }
@@ -27,7 +43,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  iosBtn: {
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    margin: 5,
+    width: 160
   }
 });
 
-export default Quiz;
+function mapStateToProps(decks) {
+  return {
+    decks
+  };
+}
+
+export default connect(mapStateToProps)(Quiz);
---
+++ components/QuizInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+export function QuizInfo({ onPress, style, text }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={style}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default QuizInfo;
