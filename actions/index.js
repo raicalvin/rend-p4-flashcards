d79b2519d596969b131a37619578956341eb079d
Calvin Rai <raicalvin@gmail.com>
2019-01-05 16:19:24 -0800

    Updated code for actions file

--- actions/index.js
+++ actions/index.js
@@ -1,18 +1,8 @@
-/* 
-    1. Getting all the decks from the store/storage
-    2. Creatnig a new deck
-    3. Creating a new card
-    4. Deleting a card
-    5. Deleting a deck
-*/
-
 // ACTIONS
 
 export const CREATE_DECK = "CREATE_DECK"; // Create a new deck
 export const ADD_CARD = "ADD_CARD"; // Add a new question card
 export const GET_DECKS = "GET_DECKS"; // Get all the decks to view on screen
-// export const DELETE_CARD = "DELETE_CARD"; // Delete a question card
-// export const DELETE_DECK = "DELETE_DECK"; // Delete a specific deck
 
 // ACTION CREATORS
 
@@ -36,17 +26,3 @@ export function addCard(card) {
     card
   };
 }
-
-// export function deleteDeck(deck) {
-//   return {
-//     type: DELETE_DECK,
-//     deck
-//   };
-// }
-
-// export function deleteCard(card) {
-//   return {
-//     type: DELETE_DECK,
-//     card
-//   };
-// }
